1be5d4e8cd4dc026b51b7f66eaf786d684fbd81b
Munawar <teramatics@gmail.com>
2017-05-24 06:05:23 +0700

    #3 commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,4 +97,27 @@
 
 	});
 
+
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/hello@greatquality.id',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
+
 })(jQuery);
